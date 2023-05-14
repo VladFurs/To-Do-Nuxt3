3ba25f315aaf4c4936108e9c3dbe77281e15ce1a
VladFurs <vlad.fursenko98@gmail.com>
2023-05-14 16:26:42 +0300

    First commit in branch

--- components/Header.vue
+++ components/Header.vue
@@ -1,16 +1,8 @@
 <script setup></script>
 
 <template>
-  <div
-  class="flex-none"
-  >
-    <div class="text-center bg-green-400 text-2xl">
-      ToDo приложение
-    </div>
-    <div class="text-center bg-green-400 text-2xl">
-      список задач на сегодня
-    </div>
+  <div class="flex-none">
+    <div class="text-center bg-green-400 text-2xl">ToDo приложение</div>
+    <div class="text-center bg-green-400 text-2xl">список задач на сегодня</div>
   </div>
 </template>
-
-
