d3032dbe4ccc35e10e8c4eb5abf53f547a8a3222
VladFurs <vlad.fursenko98@gmail.com>
2023-05-14 16:23:27 +0300

    Starn Refactoring

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,14 @@
 <script setup></script>
 
-<template
->
-  <div
-  class="flex-none">
+<template>
+  <div class="flex-none">
     <div class="copyright text-center">
       Бесплатный практикум по JavaScript от
-      <a href="https://webcademy.ru" target="_blank">ВебКадеми</a>
+      <a
+        href="https://webcademy.ru"
+        target="_blank"
+        >ВебКадеми</a
+      >
     </div>
   </div>
 </template>
